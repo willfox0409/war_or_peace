ccdaa43acc8eb36f8fb8b6dc7d8aa3dbe92d4dd0
Will Fox <william.macdonald.fox@gmail.com>
2024-10-10 20:27:05 -0400

    First Attempt at Card Test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,18 +1,17 @@
 require 'rspec'
-require './lib/card'
+require relative './lib/card'
 
 RSpec.describe Card do
-  it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
+  it 'creates a card with a suit, value, and rank' do
+    card = Card.new(:diamond, "Queen", 12)
 
-    expect(card).to be_an_instance_of(Card)
+    expect(card.suit).to eq("diamond")
+    expect(card.value).to eq("Queen")
   end
 
-  it "has readable attributes" do
+  it "returns a string description of the card" do
     card = Card.new(:diamond, 'Queen', 12)
 
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+
   end
 end
