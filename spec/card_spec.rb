7b1dff199ccedf0b3f886a9135d4b232fb4f45fc
Will Fox <william.macdonald.fox@gmail.com>
2024-10-10 21:16:11 -0400

    Updated Card Test 2

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,22 +1,23 @@
 require 'rspec'
-require relative '../card'
+require './lib/card.rb'
 
 RSpec.describe Card do
   describe '#initialize' do
   it 'creates a card with a suit, value, and rank' do
-    card = Card.new(:hearts, "king", 13)
+    card = Card.new("hearts", "king", 13)
 
     expect(card.suit).to eq("hearts")
-    expect(card.value).to eq("King")
+    expect(card.value).to eq("king")
     expect(card.rank).to eq(13)
   end
 end
 
   describe '#to_s' do
-  it 'returns correct string representation of given card'
-    card = Card.new(:hearts, 'King', 13)
+  it 'returns correct string representation of given card' do
+    card = Card.new("Hearts", "King", 13)
 
     expect(card.to_s).to eq("King of Hearts, 13")
   end
  end
-end
+end 
+
